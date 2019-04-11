f03e8597f22c223b1c22432a39b6506e315c8805
sabrinamusatian <sabrina.musatian@yandex.ru>
2019-04-11 21:02:14 +0300

    Fix MIRF tests, since test for Travis was successful

--- src/test/kotlin/features/deeplearning/tensorflow/TensorflowModelTest.kt
+++ src/test/kotlin/features/deeplearning/tensorflow/TensorflowModelTest.kt
@@ -5,9 +5,9 @@ import org.junit.Test
 class TensorflowModelTest {
 
     @Test
-    fun readDicomImageAttributesFromLocalFile_localDicom_readsWithoutErrors() {
-        val modelName = "src/test/resources/exampleDicom.dcm"
-        val va = TensorflowModelInterface(modelName,"","")
+    fun TensorflowModelInterface_loadModel_readsModelWithoutErrors() {
+        val modelName = "src/test/resources/xor_model.pb"
+        val va = TensorflowModelInterface(modelName,"my_input/X","my_output/Sigmoid")
     }
 
 }
