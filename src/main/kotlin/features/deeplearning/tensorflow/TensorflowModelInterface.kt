6fb0f7e50ca2e600f52a5cf014d005cee9e427aa
sabrinamusatian <sabrina.musatian@yandex.ru>
2019-04-12 00:48:31 +0300

    Added tests for tensorflow integration. Added some useful scripts for freezing models and for converting models from keras.

--- src/main/kotlin/features/deeplearning/tensorflow/TensorflowModelInterface.kt
+++ src/main/kotlin/features/deeplearning/tensorflow/TensorflowModelInterface.kt
@@ -1,24 +1,27 @@
 package features.deeplearning.tensorflow
 
 
-import org.tensorflow.Graph
-import org.tensorflow.Session
-import org.tensorflow.Tensor
-import org.tensorflow.TensorFlow
-import features.deeplearning.tensorflow.TensorflowInferenceInterface
+import org.tensorflow.*
+import org.tensorflow.op.core.Placeholder.shape
+
+
 
 /**
- * PbImporter is used to load and run .pb files of Tensorflow models
+ * TensorflowModelInterface is used to load and run .pb files of Tensorflow models
+ *
+ * @param modelFile is the String location of the model
+ * @param inputName is the name of the input node in tensorflow model
+ * @param outputName is the name of the output node in tensorflow model
+ * @param outputDims are the output nodes dimensions
  */
-class TensorflowModelInterface (modelFile: String, inputName: String, outputName: String) {
-    //    private static final String TAG = PbImporter.class.getSimpleName();
-    //
+class TensorflowModelInterface (modelFile: String, inputName: String, outputName: String, vararg outputDims: Int) {
     private var modelFile: String? = null
-    private var inputName: String? = null
-    private var outputName: String? = null
-    private var inferenceInterface: TensorflowInferenceInterface? = null
     private var numOfOutputDims: Int = 0
     private var numOfOutputValues: Int = 0
+
+    public var inferenceInterface: TensorflowInferenceInterface? = null
+    public var inputName: String? = null
+    public var outputName: String? = null
 
     init {
         try {
@@ -29,13 +32,11 @@ class TensorflowModelInterface (modelFile: String, inputName: String, outputName
             this.outputName = outputName
             this.numOfOutputDims = inferenceInterface.graph().operation(outputName).output<Any>(0).shape().numDimensions()
             var numOfOutputs = 1
-            for (i in 0 until numOfOutputDims) {
-                numOfOutputs *= inferenceInterface.graph().operation(outputName).output<Any>(0).shape().size(i).toInt()
-                //                Log.e(TAG, "Output for cur size: " + String.valueOf((int) inferenceInterface.graph().operation(outputName).output(0).shape().size(i)));
+            for (el in outputDims) {
+                numOfOutputs *= el.toInt()
             }
             this.numOfOutputValues = numOfOutputs
         } catch (e: UnsatisfiedLinkError) {
-            //            Log.e(TAG, "failed to load Tensorflow model: " + e.getMessage());
         }
 
     }
@@ -60,29 +61,6 @@ class TensorflowModelInterface (modelFile: String, inputName: String, outputName
             inferenceInterface!!.fetch(outputName!!, outputValues)
         } catch (e: Error) {
         }
-
         return outputValues
-    }
-
-    // Check that Tensorflow is working
-    companion object {
-        @Throws(Exception::class)
-        @JvmStatic
-        fun main(args: Array<String>) {
-            Graph().use { g ->
-                val value = "Hello from " + TensorFlow.version()
-
-                // Construct the computation graph with a single operation, a constant
-                // named "MyConst" with a value "value".
-                Tensor.create(value.toByteArray(charset("UTF-8"))).use { t ->
-                    // The Java API doesn't yet include convenience functions for adding operations.
-                    g.opBuilder("Const", "MyConst").setAttr("dtype", t.dataType()).setAttr("value", t).build()
-                }
-
-                // Execute the "MyConst" operation in a Session.
-                Session(g).use { s -> s.runner().fetch("MyConst").run()[0].use { output -> println(String(output.bytesValue())) } }// Generally, there may be multiple output tensors,
-                // all of them must be closed to prevent resource leaks.
-            }
-        }
     }
 }
--- src/test/kotlin/features/deeplearning/tensorflow/TensorflowModelTest.kt
+++ src/test/kotlin/features/deeplearning/tensorflow/TensorflowModelTest.kt
@@ -1,13 +1,64 @@
 package features.deeplearning.tensorflow
 
+import com.google.common.primitives.UnsignedBytes.toInt
+import org.junit.Assert
 import org.junit.Test
+import kotlin.math.roundToInt
+import javax.imageio.ImageIO
+import java.awt.image.BufferedImage
+import java.io.File
+import java.awt.Graphics2D
+import java.awt.Image
+import java.awt.Image.SCALE_SMOOTH
+import com.pixelmed.dicom.DicomDirectoryRecordType.image
+import java.awt.image.DataBufferFloat
+import java.awt.image.DataBufferInt
+import java.nio.IntBuffer
+
 
 class TensorflowModelTest {
 
     @Test
     fun TensorflowModelInterface_loadModel_readsModelWithoutErrors() {
         val modelName = "src/test/resources/xor_model.pb"
-        val va = TensorflowModelInterface(modelName,"my_input/X","my_output/Sigmoid")
+        val tfModel = TensorflowModelInterface(modelName,"my_input/X","my_output/Sigmoid", 1)
+        // check that input and output are set correctly
+        Assert.assertEquals("my_input/X", tfModel.inputName)
+        Assert.assertEquals("my_output/Sigmoid", tfModel.outputName)
     }
 
+    @Test
+    fun TensorflowModelInterface_runXORmodel_outputsResult() {
+        val modelName = "src/test/resources/xor_model.pb"
+        val tfModel = TensorflowModelInterface(modelName,"my_input/X","my_output/Sigmoid", 1)
+        // check that input and output are set correctly
+        var inputArr: FloatArray = floatArrayOf(1.0F, 1.0F)
+        var res = tfModel.runModel(inputArr, 1, 2)[0].roundToInt()
+        Assert.assertEquals(0, res)
+
+        inputArr  = floatArrayOf(1.0F, 0.0F)
+        res = tfModel.runModel(inputArr, 1, 2)[0].roundToInt()
+        Assert.assertEquals(1, res)
+
+        inputArr  = floatArrayOf(0.0F, 0.0F)
+        res = tfModel.runModel(inputArr, 1, 2)[0].roundToInt()
+        Assert.assertEquals(0, res)
+
+        inputArr  = floatArrayOf(0.0F, 1.0F)
+        res = tfModel.runModel(inputArr, 1, 2)[0].roundToInt()
+        Assert.assertEquals(1, res)
+    }
+
+    @Test
+    fun TensorflowModelInterface_runClassificationOnImagemodel_outputsResult() {
+        val img_size = 128
+        val modelName = "src/test/resources/tf_model.pb"
+        val tfModel = TensorflowModelInterface(modelName,"conv2d_1_input_1","activation_5_1/Sigmoid", 1, 1)
+
+        val bigArr = FloatArray(img_size * img_size * 3)
+        // check that input and output are set correctly
+        var inputArr: FloatArray = floatArrayOf(1.0F, 1.0F)
+        var res = tfModel.runModel(bigArr, 1, 128, 128, 3)[0].roundToInt()
+        Assert.assertEquals(1, res)
+    }
 }
