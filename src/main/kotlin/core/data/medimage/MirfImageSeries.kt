aad4a46c29172a589310a9c16ed67965177a9bed
avlomakin <alom2222@gmail.com>
2019-04-15 12:37:17 +0300

    fix

--- src/main/kotlin/core/data/medimage/MirfImageSeries.kt
+++ src/main/kotlin/core/data/medimage/MirfImageSeries.kt
@@ -8,6 +8,10 @@ import core.repository.RepositoryCommander
  * ImageSeries stores a list of [MedImage]
  */
 class MirfImageSeries(override val images: List<MedImage>, attributes: AttributeCollection = AttributeCollection()) : MirfData(attributes), ImageSeries {
+    override fun applyMask(masks: ImageSeries) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     override fun getFileBytes(): ByteArray {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
